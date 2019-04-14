db4c8d1a3670281e0478ca73571c0a132496a060
Timon Denisov <denisov.timon@gmail.com>
2019-04-14 22:33:39 +0300

    first lesson

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
+            while (true)
+            {
+                Turtle.Move(10);
+
+            }
+
+        }
+
+        private static void GraphicsWindow_KeyDown()
+
+        {
+            if (GraphicsWindow.LastKey == "Up")
+            {
+                Turtle.Angle = 0;
+            }
+
+            else if (GraphicsWindow.LastKey == "Right")
+            {
+                Turtle.Angle = 90;
+            }
+
+            else if (GraphicsWindow.LastKey == "Down")
+
+            {
+                Turtle.Angle = 180;
+            }
+
+            else if (GraphicsWindow.LastKey == "Left")
+            {
+                Turtle.Angle = 270;
+            }
+        }
+
+
+    }
+}
+           
